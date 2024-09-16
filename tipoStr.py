826f3577a46224ee4d800c1f3a94436b98f28e95
Alisson Filipe <alissonsantos4@estudante.ifto.edu.br>
2024-09-16 14:51:25 -0300

    aspas e barra

---
+++ tipoStr.py
@@ -0,0 +1,11 @@
+# python é de tipagem dinâmica
+# strings em todas as linguagens de programação são textos
+
+# aqui vemos um caractérie de escape
+# nesse caractere dizemos que não é pra interpretar
+print("Luiz \"Otávio")
+
+# isso daqui é utilizado para expressões regulares
+print(r"Luiz \"Otávio\"")
+
+print("aspas duplas 'dentor de aspas simples pode e vice versa'")
