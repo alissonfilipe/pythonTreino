330e756ad96de3b80abad5bf6d94de267bf54c09
Alisson Filipe <alissonsantos4@estudante.ifto.edu.br>
2024-09-16 15:18:07 -0300

    ifelseelif

---
+++ arquivosPython/ifEelseEelif.py
@@ -0,0 +1,8 @@
+entrada = input("Voce quer entrar ou sair?")
+
+if entrada == 'entrar':
+    print("vai ficar querendo")
+elif entrada == 'sair':
+    print("vai tarde")
+else:
+    print("sabe ler não mezera?")
